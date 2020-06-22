953f773829380013328c742727400fa395abf7c1
Andrew Smith <ajs3993@gmail.com>
2020-06-22 12:42:40 -0500

    signup form added

--- src/Components/Signup/Signup.js
+++ src/Components/Signup/Signup.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Signup.css";
+import {MDBInput} from 'mdbreact';
 
 class Signup extends React.Component {
 
@@ -10,7 +11,19 @@ class Signup extends React.Component {
       <div>    
     
       <h2>Sign Up</h2>
-      
+
+      <form>
+
+      Owner name: <MDBInput type="text"/>
+      Email: <MDBInput type="text"/>
+      Phone Number: <MDBInput type="text"/>
+      Dog Breed: <MDBInput type="text"/>
+      Service: <MDBInput type="text"/>
+
+      <MDBInput type="submit" value="Submit"/>
+
+      </form>
+
       </div>
     );
   }
