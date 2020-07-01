325c4e33422a94a5ca7d0bee0cc2cdb922d2a7bb
Andrew Smith <ajs3993@gmail.com>
2020-07-01 13:20:59 -0500

    remove underline from navbar

--- src/Pages/Services/Services.js
+++ src/Pages/Services/Services.js
@@ -1,18 +1,71 @@
 import React from "react";
 import "./Services.css";
+import { MDBContainer, MDBRow, MDBCol, Card } from "mdbreact";
+// import { Link } from 'react-router-dom';
 import NavBar from '../../Components/NavBar/NavBar'
 import Footer from '../../Components/Footer/Footer'
 
 const Services = props => {
   return (
-    <div className="Services">
+    <div>
       <NavBar/>
 
 <h1>Services</h1>
 
-  <Footer/>
+<div className="Services">
 
-    </div>
+<MDBContainer>
+          
+          <MDBRow>
+
+            <MDBCol sm="6">
+
+              <Card>
+
+                  <p>hello</p>
+
+              </Card>
+
+              <Card>
+
+<p>hello</p>
+
+</Card>
+
+
+              </MDBCol>
+
+              
+
+              <MDBCol sm="6">
+
+<Card>
+
+    <p>hello</p>
+
+</Card>
+
+<Card>
+
+<p>hello</p>
+
+</Card>
+
+
+</MDBCol>
+              
+              
+              
+              </MDBRow>
+              
+
+              </MDBContainer>
+              </div>
+
+             <Footer/>       
+     
+      </div>
+
   );
 };
 
